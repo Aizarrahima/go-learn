2e070c7295d9005bb88a0bd55d144fb3b4739f30
Aizar Rahima <aizarrahima@gmail.com>
2022-06-15 12:56:40 +0700

    comment

---
+++ comment.go
@@ -0,0 +1,12 @@
+package main
+
+/*
+This is multi line comment
+You can add a lot of comments here
+*/
+
+func main() {
+	//  this is single line comment
+}
+
+// Jika membuat nama variable atau function diharapkan menggunakan nama yang mudah dipahami dan dibaca oleh programmer lain
