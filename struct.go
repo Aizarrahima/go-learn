7e7f5f2ed52bb4be9c9e78be1f0142dbb3721cda
Aizar Rahima <aizarrahima@gmail.com>
2022-06-15 14:43:58 +0700

    struct method

--- struct.go
+++ struct.go
@@ -7,20 +7,37 @@ type Customer struct {
 	Age           int
 }
 
+// func sayHi(customer Customer, name string){
+// 	fmt.Println("Hello", name, "My Name is", customer.Name)
+// }
+
+func (customer Customer) sayHi(name string){
+	fmt.Println("Hello", name, "My Name is", customer.Name)
+}
+
+func (a Customer) sayHello(){
+	fmt.Println("Hello from", a.Name)
+}
+
 func main() {
 	var aizar Customer
 	aizar.Name = "Aizar Rahima"
 	aizar.Address = "Indonesia"
 	aizar.Age = 18
 
-	fmt.Println(aizar.Name)
-	fmt.Println(aizar.Address)
-	fmt.Println(aizar.Age)
+	// sayHi(aizar, "Lisa")
 
-	adinda := Customer {
-		Name: "Adinda",
-		Address: "Malang",
-		Age: 18,
-	} 
-	fmt.Println(adinda)
+	aizar.sayHi("Lisa")
+	aizar.sayHello()
+
+	// fmt.Println(aizar.Name)
+	// fmt.Println(aizar.Address)
+	// fmt.Println(aizar.Age)
+
+	// adinda := Customer {
+	// 	Name: "Adinda",
+	// 	Address: "Malang",
+	// 	Age: 18,
+	// } 
+	// fmt.Println(adinda)
 }
