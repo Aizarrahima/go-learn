8592a42da232f6c464a8bff2a7f690b183bb19ac
Aizar Rahima <aizarrahima@gmail.com>
2022-06-17 12:17:27 +0700

    Access Modifier

--- helper/sayhello.go
+++ helper/sayhello.go
@@ -2,6 +2,14 @@ package helper
 
 import "fmt"
 
+var version = 1
+var Application = "Belajar Golang"
+
 func SayHello(name string) {
 	fmt.Println("Hello", name)
+}
+
+// func yang tidak bisa diakses diluar package
+func sayGoodBye(name string) {
+	fmt.Println("Goodbye", name)
 }
--- import.go
+++ import.go
@@ -1,7 +1,16 @@
 package main
 
-import "go-learn/helper"
+import (
+	"fmt"
+	"go-learn/helper"
+)
 
 func main() {
 	helper.SayHello("Aizar")
+
+	// tidak bisa diakses karena awalan nama function menggunakan huruf kecil
+	// helper.sayGoodBye("Aizar") // error
+
+	fmt.Println(helper.Application)
+	// fmt.Println(helper.version) // error, karena menggunakan awalan nama variable menggunakan huruf kecil
 }
